a6cbb3efbf475958485e7112088cad0b278b4f54
LucaB98 <lucabellan98@gmail.com>
2024-02-19 17:56:34 +0100

    finish second snack

--- script.php
+++ script.php
@@ -3,7 +3,7 @@ $name = $_GET['name'] ?? '';
 $age = $_GET['age'] ?? '';
 $email = $_GET['email'] ?? '';
 
-if(strlen($name) <= 4 || stripos($email,'. @') !== false || is_nan($age)){
+if(strlen($name) <= 4 || stripos($email,'. @') !== false || is_numeric($age)){
     echo 'Accesso eseguito';
 }else{
     echo 'Accesso negato';
